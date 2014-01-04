8492541a6748d130144f6530e055f4cec5c4b87c
familia <familia@familia.(none)>
2014-01-04 12:42:20 -0500

    Creación de CRUD nota

---
+++ src/Colegio/EstudianteBundle/Form/NotaType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Colegio\EstudianteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class NotaType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('idAsignatura')
+            ->add('calificacion')
+            ->add('descripcion')
+            ->add('fecha')
+            ->add('idEstudiante')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Colegio\EstudianteBundle\Entity\Nota'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'colegio_estudiantebundle_nota';
+    }
+}
